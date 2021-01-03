6edb50419c6bc1f46a63080ba59f4e77b2643540
Katrina <katrinajara2@gmail.com>
2021-01-03 12:15:19 -0500

    fixed event listener

--- static/app.js
+++ static/app.js
@@ -3,12 +3,15 @@ $(function(){
     alert("am I working?")
 
     const $submitWord=$("#submit-word");
-    const $wordGuess=$("#word-guess");
+    const $wordGuess=$("#word-guess"); //want to send this value to the server (on a new route?)
     
     $("#submit-word").on("submit", async function(e){
         e.preventDefault()
         console.log('testing')
+
         // await axios.post('/word-guess')
+
+        $('#word-guess').val()='' //clear out value
     })
 
 })
