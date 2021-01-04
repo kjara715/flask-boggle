7b5b49d7cb83a5bc812b258e5aadb3a3c872db83
Katrina <katrinajara2@gmail.com>
2021-01-04 16:14:29 -0500

    version with timer, score display, no highscore functionality yet

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, session, flash
+from flask import Flask, request, render_template, session, flash, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_cors import CORS, cross_origin
 
@@ -15,21 +15,20 @@ boggle_game = Boggle()
 
 @app.route('/')
 def game_page():
-    session['board'] = boggle_game.make_board()
-    return render_template("home.html")
+    board=boggle_game.make_board()
+    session['board'] = board
+    highscore=session.get("highscore", 0) #if no highscore, then the highscore is set to 0
+    nplays=session.get("nplays", 0) #if no plays, then the highscore is set to 0
+    return render_template("home.html", highscore=highscore, nplays=nplays)
 
 @app.route('/word-guess')
-@cross_origin()
 def check_word():
-    my_word = request.args["word-guess"] #I think it's based on the id so this should work, should give the form value, which is the word that was typed in
-    all_words=boggle_game.words #gives list of all words in our dictionary
-    if my_word not in all_words:
-        flash("Not a valid word")
-        return redirect('/')
-
-#     result = boggle_game.check_valid_word(session['board'], my_word) #pass in the board and the word to check
-#     if result is "ok"
-#         flash('is word')
+    my_word=request.args['word']
+    board=session["board"]
+    result = boggle_game.check_valid_word(board, my_word) #pass in the board and the word to check
     
+    return jsonify({"result": result})
 
-
+# @app.route('/score', methods=["POST", "GET"])
+# def post_score():
+#     score= request.json['currentScore'] #why not request.form?
--- static/app.js
+++ static/app.js
@@ -1,13 +1,46 @@
 $(function(){
-    
+    let score=0
     $("#submit-word").on("submit", async function(e){
         e.preventDefault() //prevents page refresh
         console.log('testing')
 
-        await axios.get('/word-guess') //want to send a post request to the server without refreshing the page
 
-        // console.log($('#word-guess').val()) 
-        // $('#word-guess').val('')
+        const wordGuess=$('#word-guess').val()
+        const response = await axios.get('/word-guess', {params:{
+            word: wordGuess
+        }
+    })
+        
+        if(response.data.result === "not-word"){
+            $(".message").text(`${wordGuess} is not an english word`)
+        } else if (response.data.result === "not-on-board"){
+            $(".message").text(`${wordGuess} is not on this board`)
+        } else {
+            $(".message").text(`${wordGuess} is a valid word on this board`)
+            score+=wordGuess.length;
+            $(".score").text(`${score}`)
+        }
+
+        $('#word-guess').val('')
+    }) //want to send a post request to the server without refreshing the page
+    
+    let count=60
+    let timer = setInterval(function(){
+        count--
+        $(".timer").text(`${count}`)
+        if(count === 0){
+            alert(`Your time is up! Your final score is ${score}`)
+            clearInterval(timer)
+            $('#submit-word').off()
+        }
+    }, 1000)
     })
 
-})
+    // if(count === 0){
+    //     const response await axios.post('/score', {params: {
+    //         currentScore: score
+    //     }})
+
+    // }
+
+
